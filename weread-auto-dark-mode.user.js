c9db3ca002de46132b720ee837895de9a67ddc53
Chiang <sherlockchiang@gmail.com>
2026-04-08 19:25:38 +0800

    Update metadata for weread-auto-dark-mode script

--- weread-auto-dark-mode.user.js
+++ weread-auto-dark-mode.user.js
@@ -1,10 +1,13 @@
 // ==UserScript==
 // @name         微信读书自动夜间模式
-// @namespace    http://tampermonkey.net/
+// @namespace    https://github.com/SherlockChiang/Weread-auto-darkmode
 // @version      1.0
 // @description  根据系统深色模式自动切换微信读书的黑夜/白天主题
 // @author       Uranium92
 // @match        *://weread.qq.com/*
+// @icon         https://weread.qq.com/favicon.ico
+// @updateURL    https://https://raw.githubusercontent.com/SherlockChiang/Weread-auto-darkmode/main/weread-auto-dark-mode.user.js
+// @downloadURL  https://https://raw.githubusercontent.com/SherlockChiang/Weread-auto-darkmode/main/weread-auto-dark-mode.user.js
 // @grant        none
 // ==/UserScript==
 
